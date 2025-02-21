6aa3097b1c47b5f19b54b5a1a18dc35e564e839a
Brandon K. Waltz <bwaltz6@gmail.com>
2025-02-21 16:03:33 -0500

    updates

--- wp-config.php
+++ wp-config.php
@@ -9,4 +9,5 @@ define('PLATFORM_VARIABLES', json_decode($_ENV['PLATFORM_VARS'], true));
 define( 'ALGOLIA_INDEX_NAME_PREFIX', 'kinsta_colbyarts_' );
 define('WP_CONTENT_DIR', dirname(__FILE__) . '/web/wp-content');
 
+// NOTICE: Don't forget to comment out the wp-settings require_once in wp-config-kinsta.php
 include_once dirname( __FILE__ ) . '/wp-config-kinsta.php';
