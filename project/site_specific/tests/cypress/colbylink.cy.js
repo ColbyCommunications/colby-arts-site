29bdbfd525840db7d33e2fe65e4c76eaccdc536d
Brandon K. Waltz <bwaltz6@gmail.com>
2025-07-01 15:22:38 -0400

    Update colbylink.cy.js

--- project/site_specific/tests/cypress/colbylink.cy.js
+++ project/site_specific/tests/cypress/colbylink.cy.js
@@ -2,6 +2,8 @@ describe('Colby Link', () => {
     it('Should go to Colby homepage', () => {
         cy.visit('/');
         cy.get('.header__utility > ul > li:first-child a').click({ force: true });
-        cy.location('hostname', { timeout: 3000 }).should('include', 'www.colby.edu');
+        cy.origin('www.colby.edu', () => {
+            cy.location('hostname', { timeout: 3000 }).should('include', 'www.colby.edu');
+        });
     });
 });
